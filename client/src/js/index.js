6e164d5076624a8c13bf851abb1670d0d7969b04
Jose Plasencia <josejpd3@gmail.com>
2022-12-31 12:30:22 -0500

    commented unused js (client/src/js/index.js)

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,18 +1,18 @@
 import '../css/style.css';
 
-const main = document.querySelector('#main');
-main.innerHTML = '';
+// const main = document.querySelector('#main');
+// main.innerHTML = '';
 
-const loadSpinner = () => {
-  const spinner = document.createElement('div');
-  spinner.classList.add('spinner');
-  spinner.innerHTML = `
-  <div class="loading-container">
-  <div class="loading-spinner" />
-  </div>
-  `;
-  main.appendChild(spinner);
-};
+// const loadSpinner = () => {
+//   const spinner = document.createElement('div');
+//   spinner.classList.add('spinner');
+//   spinner.innerHTML = `
+//   <div class="loading-container">
+//   <div class="loading-spinner" />
+//   </div>
+//   `;
+//   main.appendChild(spinner);
+// };
 
 
 
